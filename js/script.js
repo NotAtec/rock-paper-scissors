b8fda3e2dc05c6a3bab9f12c5fe8855df4cb397a
Jeroen B <jeroenb04@live.com>
2021-12-20 01:17:03 +0100

    Setup initial UI Buttons & event listeners added, with a setup function

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        return 2;
+        return 'tie';
     } else {
         switch (playerSelection) {
             case 'rock':
@@ -38,27 +38,34 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    for (let i=0; i < 5; i++) {
-        let computerChoice = computerPlay();
-        let playerChoice = prompt('What do you choose? (Rock, Paper, Scissors)');
-        let result = playRound(playerChoice, computerChoice);
-
-        switch (result) {
-            case 'loss':
-                console.log(`You Lose! ${playerChoice} gets beaten by ${computerChoice}!.`);
-                computerScore += 1
-                break;
-            case 'win':
-                console.log(`You Win! ${playerChoice} beats ${computerChoice}.`);
-                playerScore += 1
-                break;
-            default:
-                console.log(`It's a Tie! You and the computer chose ${playerChoice}.`);
-        }
-        console.log(`Current Score: You: ${playerScore}, Computer: ${computerScore}`);
-    }
+function setupRound(e) {
+    const computerSelection = computerPlay();
+    const playerSelection = e.target.id;
+    playRound(playerSelection, computerSelection);
 }
+//function game() {
+//    let computerScore = 0;
+//    let playerScore = 0;
+//    for (let i=0; i < 5; i++) {
+//        let computerChoice = computerPlay();
+//        let playerChoice = prompt('What do you choose? (Rock, Paper, Scissors)');
+//        let result = playRound(playerChoice, computerChoice);
+//
+//        switch (result) {
+//            case 'loss':
+//                console.log(`You Lose! ${playerChoice} gets beaten by ${computerChoice}!.`);
+//                computerScore += 1
+//                break;
+//            case 'win':
+//              console.log(`You Win! ${playerChoice} beats ${computerChoice}.`);
+//                playerScore += 1
+//                break;
+//            default:
+//                console.log(`It's a Tie! You and the computer chose ${playerChoice}.`);
+//        }
+//        console.log(`Current Score: You: ${playerScore}, Computer: ${computerScore}`);
+//    }
+//}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => button.addEventListener('click', setupRound));
